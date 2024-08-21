3493978a9614ed287b357e48fced4e7431cb1ed2
luiz miguel <lff32337@gmail.com>
2024-08-21 20:45:55 -0300

    ex04.py
    add exercicio 04, feito com amor e carinho

---
+++ ex04.py
@@ -0,0 +1,11 @@
+# receba dois numeros ao usuario
+nm1 = float(input("escreva o primeiro digito:"))
+nm2 = float(input("escreva o segundo digito: "))
+# encontre o maior numero
+if nm1 >nm2:
+    maior = nm1
+    
+else: 
+    maior = nm2
+    # esxiba o maior numero
+    print(f"o mairo numero é: { maior }" ) 
