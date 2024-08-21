3144fdbb29fefec65828de00084b012834382d9a
luiz miguel <lff32337@gmail.com>
2024-08-21 20:28:42 -0300

    ex03.py
    add o exercicio 03, feito com muito amor e carinho :)

---
+++ ex03.py
@@ -0,0 +1,7 @@
+nm1 = float(input("escreva o primeiro digito: "))
+nm2 = float(input("escreva o segundo digito: "))
+nm3 = float(input("escreva o terceiro digito: ")) 
+# realize a soma dos tres numeros
+soma = nm1 + nm2 + nm3  
+# exiba o resultado
+print ("o valor foi: ",soma)
