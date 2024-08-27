363e9f96edb432d31cd2b2193e01e6de05be05bb
luiz miguel <lff32337@gmail.com>
2024-08-27 18:31:07 -0300

    ex05.py
    add exercicio 05, feito com muito amor e carinho

---
+++ ex05.py
@@ -0,0 +1,10 @@
+# Programa para verificar se uma pessoa é maior ou menor de idade
+
+# Solicita a idade ao usuário
+idade = int(input("Digite a sua idade: "))
+
+# Verifica se a idade é maior ou menor de idade
+if idade >= 18:
+    print("Você é maior de idade.")
+else:
+    print("Você é menor de idade.")
