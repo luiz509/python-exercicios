f00956eaba535709287f717d70769bd85005b44a
luiz miguel <lff32337@gmail.com>
2024-08-27 20:37:19 -0300

    ex24.py
    add exercicio 25, feito com muito amor e carinho

---
+++ ex24.py
@@ -0,0 +1,8 @@
+# Entrada do usuário: lado do quadrado
+lado = float(input("Digite o valor do lado do quadrado: "))
+
+# Cálculo da área do quadrado
+area = lado * lado
+
+# Saída do resultado
+print("A área do quadrado é:", area)
