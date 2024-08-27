f37dcf14f6181a8be55676cd6b7c3ccb63d87159
luiz miguel <lff32337@gmail.com>
2024-08-27 20:32:45 -0300

    ex23.py
    add exercicio 23, feito com muito amor e carinho :)

---
+++ ex23.py
@@ -0,0 +1,9 @@
+# Entrada do usuário: base e altura do triângulo
+base = float(input("Digite a base do triângulo: "))
+altura = float(input("Digite a altura do triângulo: "))
+
+# Cálculo da área do triângulo
+area = (base * altura) / 2
+
+# Saída do resultado
+print("A área do triângulo é:", area)
