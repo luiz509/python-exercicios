12afeb3eb57fa5a472b070b0de682c182e89d459
luiz miguel <lff32337@gmail.com>
2024-08-27 20:11:57 -0300

    ex15.py
    add exercicio 15, feito com muito amor e carinho :)

---
+++ ex15.py
@@ -0,0 +1,11 @@
+# Entrada do usuário: salário atual
+salario = float(input("Digite o salário atual do funcionário: "))
+
+# Cálculo do aumento de 25%
+aumento = salario * 0.25
+
+# Cálculo do novo salário
+novo_salario = salario + aumento
+
+# Saída do resultado
+print("O novo salário é:", novo_salario)
