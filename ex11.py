b3be40bd5118eb5df2b9ac2f9c07b03aad5998b5
luiz miguel <lff32337@gmail.com>
2024-08-27 18:44:36 -0300

    ex11.py
    add exercicio 11, feito com muito amor e carinho

---
+++ ex11.py
@@ -0,0 +1,10 @@
+# Programa para verificar se um número é par ou ímpar
+
+# Recebe um número inteiro do usuário
+numero = int(input("Digite um número inteiro: "))
+
+# Verifica se o número é par ou ímpar
+if numero % 2 == 0:
+    print(f"O número {numero} é par.")
+else:
+    print(f"O número {numero} é ímpar.")
