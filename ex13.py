264ace67c51c45f81495eb4a8a06b5c8662d5ec5
luiz miguel <lff32337@gmail.com>
2024-08-27 18:48:36 -0300

    ex13.py
    add exercicio 13, feito com muito amor e carinho :)

---
+++ ex13.py
@@ -0,0 +1,8 @@
+# Entrada do usuário em metros
+metros = float(input("Digite o valor em metros: "))
+
+# Conversão de metros para centímetros
+centimetros = metros * 100
+
+# Saída do resultado
+print("O valor em centímetros é:", centimetros)
